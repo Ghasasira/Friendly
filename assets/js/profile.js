be5e017727fece6ab405e6f5d64c1ddd13b821ec
Ghasasira <gasasiraj013@outlook.com>
2022-09-28 13:22:23 +0300

    Please enter the commit message for your changes. Lines starting with '#' will be ignored, and an empty message aborts the commit.
    On branch main
    Your branch is up to date with 'origin/main'.
    
    Changes to be committed:
    	modified:   assets/js/profile.js
    	modified:   landing.html

--- assets/js/profile.js
+++ assets/js/profile.js
@@ -4,6 +4,7 @@ const updateProfile = e => {
     let //dp = document.getElementById('pp').src || null,
         username = document.getElementById('username').value,
         statement = document.getElementById('statement').value,
+        Address = document.getElementById('location').value,
         primoeducation = document.getElementById('primoschool').value,
         seceducation = document.getElementById('secschool').value,
         campuseducation = document.getElementById('muk').value || null,
@@ -15,6 +16,22 @@ const updateProfile = e => {
     any = document.getElementById('aob').value || null;
 
 
+    //Setting profile attributes as user enters details
+    document.getElementById("displayusername").innerHTML = username;
+    document.getElementById("displaystatement").innerHTML = statement;
+    document.getElementById("address").innerHTML = Address;
+    document.getElementById("primo").innerHTML = primoeducation;
+    document.getElementById("high").innerHTML = seceducation;
+    document.getElementById("campus").innerHTML = campuseducation;
+    document.getElementById("hobb1").innerHTML = hobbies1;
+    document.getElementById("hobb2").innerHTML = hobbies2;
+    document.getElementById("hobb3").innerHTML = hobbies3;
+    document.getElementById("hobb4").innerHTML = hobbies4;
+    document.getElementById("hobb5").innerHTML = hobbies5;
+
+
+
+    //Storage of the data
     let user = {
         //img: dp,
         name: username,
@@ -39,9 +56,9 @@ const updateProfile = e => {
         users.push(user);
         sessionStorage.setItem("users", JSON.stringify(users));
     }
-    sessionStorage.setItem("currentUser", username.value);
+    //sessionStorage.setItem("currentUser", username.value);
     sessionStorage.setItem("currentUserDetails", JSON.stringify(user));
-    //console.log(JSON.parse(sessionStorage.getItem("users[1]")))
+
     e.preventDefault();
 }
 
@@ -51,21 +68,24 @@ const updateProfile = e => {
 
 
 function previewImage() {
-    var profilepic = document.getElementById('profile');
+    var profilepic = document.getElementById('displaypic');
+    var mainpic = document.getElementById('pico');
     var actualPic = document.getElementById('pp').files;
     var reader = new FileReader();
     if (actualPic.length > 0) {
 
         reader.onload = function(e) {
             profilepic.setAttribute("src", e.target.result);
+            mainpic.setAttribute("src", e.target.result);
         };
 
 
     } else {
         //var reader=new FileReader();
         //reader.onload = function(e) {
-        profilepic.setAttribute("src", "avatar.png");
-        //}
+        profilepic.setAttribute("src", "assets/img/profile-img.png");
+        mainpic.setAttribute("src", "assets/img/profile-img.png")
+            //}
     }
     reader.readAsDataURL(actualPic[0]);
 }
